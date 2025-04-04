4214dda0a63835f28ae37cf89004f5d00839c9de
dima55279 <dima55279@yandex.ru>
2025-04-05 00:48:50 +0300

    Выполнено задание 5.

--- solutions/5-constructor.js
+++ solutions/5-constructor.js
@@ -1,3 +1,29 @@
 // BEGIN
-
+function getX () {
+    return this.x;
+}
+function getY () {
+    return this.y;
+}
+export function Point (x, y) {
+    this.x = x;
+    this.y = y;
+    this.getX = getX;
+    this.getY = getY;
+}
+function getBeginPoint () {
+    return this.beginPoint;
+}
+function getEndPoint () {
+    return this.endPoint;
+}
+export function Segment (beginPoint, endPoint) {
+    this.beginPoint = new Point(beginPoint.getX(), beginPoint.getY());
+    this.endPoint = new Point(endPoint.getX(), endPoint.getY());
+    this.getBeginPoint = getBeginPoint;
+    this.getEndPoint = getEndPoint;
+}
+export const reverse = (segment) => {
+    return new Segment (segment.getEndPoint(), segment.getBeginPoint());
+};
 // END
