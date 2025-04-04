e8fa7a3350555556c30ba9ea6dbdec52cb450d9f
dima55279 <dima55279@yandex.ru>
2025-04-04 12:11:24 +0300

    Выполнено задание 1.

--- solutions/1-encapsulation.js
+++ solutions/1-encapsulation.js
@@ -1,5 +1,14 @@
 // BEGIN
-
+export const getMutualFriends = (firstUser, secondUser) => {
+    const result = [];
+    firstUser.getFriends().forEach((friendOfFirstUser) => {
+      secondUser.getFriends().forEach((friendOfSecondUser) => {
+        if (friendOfFirstUser.id === friendOfSecondUser.id)
+          result.push(friendOfFirstUser);
+      });
+    });
+    return result;
+}
 // END
 
 export default ({ id = null, friends = [] } = {}) => ({
