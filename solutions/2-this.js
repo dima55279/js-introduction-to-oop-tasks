8bdbbb6eb7c67282d1794edc36bf0e83d1496350
dima55279 <dima55279@yandex.ru>
2025-04-04 12:25:43 +0300

    Выполнено задание 2.

--- solutions/2-this.js
+++ solutions/2-this.js
@@ -1,3 +1,27 @@
 // BEGIN
-
+const make = (numer = null, denom = null) => {
+    return {
+        numer: numer,
+        denom: denom,
+        setNumer(value) {
+            this.numer = value;
+        },
+        setDenom(value) {
+            this.denom = value;
+        },
+        getNumer() {
+            return this.numer;
+        },
+        getDenom() {
+            return this.denom;
+        },
+        toString() {
+            return `${this.numer}/${this.denom}`;
+        },
+        add(rat) {
+            return make((this.numer * rat.getDenom() + this.denom * rat.getNumer()), (this.denom * rat.getDenom()));
+        }
+    }
+}
+export default make;
 // END
