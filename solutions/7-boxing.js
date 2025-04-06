13092ef16f2008776338c5b26b8a7ae207e955b6
dima55279 <dima55279@yandex.ru>
2025-04-06 14:26:56 +0300

    Выполнено задание 7.

--- solutions/7-boxing.js
+++ solutions/7-boxing.js
@@ -1,3 +1,11 @@
 // BEGIN
-
+const magic = (...numbers) => {
+    const result = numbers.reduce((sum, number) => sum + number, 0);
+    const newMagic = (...newNumbers) => {
+        return magic(...numbers, ...newNumbers);
+    }
+    newMagic.valueOf = () => result;
+    return newMagic;
+}
+export default magic;
 // END
