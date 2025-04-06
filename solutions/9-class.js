ea9121f7bc71b5be7ed85dd380208a756df47851
dima55279 <dima55279@yandex.ru>
2025-04-06 14:49:13 +0300

    Выполнено задание 9.

--- solutions/9-class.js
+++ solutions/9-class.js
@@ -1,5 +1,31 @@
 import _ from 'lodash';
 
 // BEGIN
-
+class Cart {
+    constructor() {
+        this.items = [];
+    }
+    addItem (item, count) {
+        const product = {item: item, count: count}
+        return this.items.push(product);
+    };
+    getItems () {
+        return _.cloneDeep(this.items);
+    };
+    getCost () {
+        let sum = 0;
+        this.items.forEach(product => {
+            sum += product.item.price * product.count;
+        });
+        return sum;
+    };
+    getCount () {
+        let counter = 0;
+        this.items.forEach(product => {
+            counter += product.count;
+        });
+        return counter;
+    };
+}
+export default Cart;
 // END
